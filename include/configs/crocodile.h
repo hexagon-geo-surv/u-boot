43d6a317d643be34a36606176177ee8276bc573b
Thomas Haemmerle <thomas.haemmerle@leica-geosystems.com>
2022-04-20 11:29:17 +0000

    crocodile: fix size of memory
    We have only 1GB of memory on crocodile.
    
    Signed-off-by: Thomas Haemmerle <thomas.haemmerle@leica-geosystems.com>

--- include/configs/crocodile.h
+++ include/configs/crocodile.h
@@ -93,7 +93,7 @@
 
 #define CONFIG_SYS_SDRAM_BASE           0x40000000
 #define PHYS_SDRAM                      0x40000000
-#define PHYS_SDRAM_SIZE			0x80000000 /* 2GB DDR */
+#define PHYS_SDRAM_SIZE			0x40000000 /* 1GB DDR */
 
 #define CONFIG_MXC_UART_BASE		UART3_BASE_ADDR
 
